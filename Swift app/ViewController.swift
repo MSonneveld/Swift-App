c58108cd78df04b6316254d47f48355b650f741f
Mickey Sonneveld <mickeysonneveld@Mickeys-MacBook-Pro.local>
2016-11-09 18:54:15 +0100

    my first commit

---
+++ Swift app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift app
+//
+//  Created by Mickey Sonneveld on 09/11/16.
+//  Copyright © 2016 Mickey Sonneveld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        theLabel.text = "Hello There!"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
